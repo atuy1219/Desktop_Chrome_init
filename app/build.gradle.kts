144c9c1f805fcf0fa8406bc460e09170fa2f6c3a
Atuy <130917473+atuy1219@users.noreply.github.com>
2026-09-19 16:51:51 +0900

    Bump version to 0.3.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.atuy.desktopchromeinit"
         minSdk = 29
         targetSdk = 35
-        versionCode = 17
-        versionName = "0.3.3"
+        versionCode = 18
+        versionName = "0.3.4"
     }
 
     signingConfigs {
